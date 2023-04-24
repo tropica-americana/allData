8f22d5fdfe4beb4536ce9c9780e9dbfb27cadcbe
sachin jain <sachinjain504.com>
2023-04-25 00:54:01 +0530

    further task additions

--- semiconductors/addToAllData.sh
+++ semiconductors/addToAllData.sh
@@ -5,7 +5,8 @@ shift
 ASSOCIATE="asc"
 C_PLUS_PLUS="cpp"
 CVS="cvs"
-VALID_INPUTS=($ASSOCIATE $C_PLUS_PLUS $CVS)
+OPEN="open"
+VALID_INPUTS=($ASSOCIATE $C_PLUS_PLUS $CVS $OPEN )
 USER_INPUT="-------------------------------------------------\n\n$@\n\n-------------------------------------------------"
 
 if [[ $INPUT == $ASSOCIATE ]]; then
@@ -17,8 +18,12 @@ elif [[ $INPUT == $C_PLUS_PLUS ]]; then
 elif [[ $INPUT == $CVS ]]; then
     echo -e $USER_INPUT >> /Users/sachinjain/Desktop/AllData/medicine/uworld/CVS.txt
 
+elif [[ $INPUT == $OPEN ]]; then
+    open /Users/sachinjain/Desktop/AllData/semiconductors/addToAllData.sh
+
 else
     echo "invalid input"
     echo "valid inputs are --> ${VALID_INPUTS[@]}"
 
 fi
+
---
+++ semiconductors/taskUpdate.sh
@@ -0,0 +1,20 @@
+#! /bin/bash 
+
+TEXT_FILE="/Users/sachinjain/desktop/alldata/dailyEventsAndQuotations/task.txt"
+if [ $1 = "show" ]
+then
+    echo $(cat $TEXT_FILE)
+    # where cat means concatenate
+    # basically it means to print the contents of the file
+fi
+if [ $1 = "add" ]
+then
+    shift 
+    echo $@ > /Users/sachinjain/desktop/alldata/dailyEventsAndQuotations/task.txt
+fi
+#  if there is no passed parameters then it will show the contents of the file
+if [ $# -eq 0 ]
+then
+    echo $(cat $TEXT_FILE)
+fi
+# where $# means the number of parameters passed
