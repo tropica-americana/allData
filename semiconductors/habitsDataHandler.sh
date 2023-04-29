eb1b0387d4625c54fc0bcf01c9d78ad843d652f4
sachin jain <sachinjain504.com>
2023-04-29 13:21:21 +0530

    insane habits shell script made by gpt

---
+++ semiconductors/habitsDataHandler.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+HABITTEXTFILE="/Users/sachinjain/Desktop/AllData/dailyEventsAndQuotations/habitsData.txt"
+TEMPFILE="/Users/sachinjain/Desktop/AllData/dailyEventsAndQuotations/temp.txt"
+ROD="-----------------------------------------------------------------------"
+SMALLROD="--------------"
+TODAYS_DATE=$(date "+%Y-%m-%d")
+
+# check whether the $1 is open 
+if [ "$1" == "open" ]; then
+    code $HABITTEXTFILE
+    exit 0
+fi
+if [ "$1" == "code" ]; then
+    code /Users/sachinjain/Desktop/AllData/semiconductors/habitsDataHandler.sh
+    exit 0
+fi
+if [ "$1" == "cat" ]; then
+    /Users/sachinjain/Desktop/AllData/semiconductors/bionify.sh $HABITTEXTFILE
+    exit 0
+fi
+# Function to create the template
+create_template() {
+    echo $ROD >> $HABITTEXTFILE
+    echo $TODAYS_DATE >> $HABITTEXTFILE
+    for i in {1..10}; do
+        echo $SMALLROD >> $HABITTEXTFILE
+        echo "HABIT $i" >> $HABITTEXTFILE
+    done
+}
+
+# Search for today's date in the file
+grep -q -F "$TODAYS_DATE" "$HABITTEXTFILE"
+
+# Check the exit status of grep to see if the date was found
+if [ $? -ne 0 ]; then
+    create_template
+fi
+
+# Extract the habit number and the event description
+habit_number=$(echo $1 | grep -o -E '[0-9]+')
+event_description=${@:2}
+
+# Append the input to the corresponding habit using awk
+awk -v date="$TODAYS_DATE" -v rod="$ROD" -v habit_number="$habit_number" -v event_description="event \"$event_description\"" '
+BEGIN {OFS=FS=""}
+$0 ~ date, $0 ~ rod {
+    if ($0 ~ "HABIT " habit_number) {
+        print $0; print event_description; next
+    }
+}
+{print}' "$HABITTEXTFILE" > "$TEMPFILE" && mv "$TEMPFILE" "$HABITTEXTFILE"
--- semiconductors/stockInAShell.sh
+++ semiconductors/stockInAShell.sh
@@ -1,5 +1,12 @@
 INPUT=$1
 ONGC="ongc"
+OCCI="occi"
+if [ $# -eq 0 ]
+then
+    echo "valid options are"
+    echo $ONGC $OCCI 
+    exit 1
+fi
 if [[ $INPUT == $ONGC ]]
 then
     awk 'BEGIN  { OFS="             "} {
@@ -11,3 +18,15 @@ then
         }
     }' /Users/sachinjain/Desktop/AllData/stocks/ongc.txt
 fi
+if [[ $INPUT == $OCCI ]]
+then
+    awk 'BEGIN  { OFS="             "} {
+        for(i=2; i<=NF; i++) {
+            tmp=match($i,/^[+]?[0-9]*\.?[0-9]+$/)
+            if (tmp) {
+                print $1
+            }
+        }
+    }' /Users/sachinjain/Desktop/AllData/stocks/occidental.txt
+fi
+
